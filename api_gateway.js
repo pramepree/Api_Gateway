bc2a3d3838d5aabba89abf20959101661ca27302
pramepree <pramepree159@gmail.com>
2024-06-10 17:24:00 +0700

    update

--- api_gateway.js
+++ api_gateway.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+const multer = require('multer');
 
 const app = express();
 app.use(express.json());
@@ -25,13 +26,40 @@ app.post('/post_NameMember', async (req, res) => {
   try {
     // ส่งคำขอไปยัง API ภายนอก
     const response = await axios.post('http://localhost:3000/post_NameMember', req.body);
-    
+
     // ส่งค่าตอบกลับจาก API ภายนอกมายังผู้เรียกใช้
     res.status(response.status).json(response.data);
     console.log(response.data);
   } catch (error) {
     // จัดการข้อผิดพลาดและส่งข้อความกลับไปยังผู้เรียกใช้
     res.status(500).send('Failed to post data to API 1');
+  }
+});
+
+
+const upload = multer({ dest: 'uploads/' });
+app.post('/uploadimages', upload.array('images', 12), async (req, res) => {
+  try {
+    const files = req.files;
+    if (!files || files.length === 0) {
+      return res.status(400).send('No files uploaded.');
+    }
+
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append('images', file.buffer, file.originalname);
+    });
+
+    const response = await axios.post('http://localhost:3000/uploadimages', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error uploading files.');
   }
 });
 
